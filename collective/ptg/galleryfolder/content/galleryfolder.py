22d81ae132f263e12b793294cef1265ffcb15c01
Espen Moe-Nilssen <espen@medialog.no>
2012-11-30 17:14:32 +0100

    i18 change

--- collective/ptg/galleryfolder/content/galleryfolder.py
+++ collective/ptg/galleryfolder/content/galleryfolder.py
@@ -4,11 +4,30 @@ from Products.Archetypes import atapi
 from Products.ATContentTypes.content import folder
 from collective.ptg.galleryfolder.interfaces import IGalleryfolder
 from collective.ptg.galleryfolder.config import PROJECTNAME
+from Products.ATContentTypes.content import schemata
+
+
+from zope.interface import implements
+
+GalleryfolderSchema = folder.ATFolderSchema.copy() 
+
+GalleryfolderSchema['title'].storage = atapi.AnnotationStorage()
+GalleryfolderSchema['description'].storage = atapi.AnnotationStorage()
+
+
+schemata.finalizeATCTSchema(GalleryfolderSchema, moveDiscussion=False)
+
 
 
 class Galleryfolder(folder.ATFolder):
     """Galleryfolder is a copy of a normal folder"""
+    implements(IGalleryfolder)
+
+    meta_type = "Galleryfolder"
+    schema = GalleryfolderSchema
+
+    title = atapi.ATFieldProperty('title')
+    description = atapi.ATFieldProperty('description')
 
 
-#registerATCT(Galleryfolder, PROJECTNAME)    
 atapi.registerType(Galleryfolder, PROJECTNAME)
